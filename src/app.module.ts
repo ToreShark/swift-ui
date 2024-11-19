8d8d95e11fdb7f9fceb29b388b6836ca6107133a
torekhanmukhtarov <nahkerot@gmail.com>
2024-11-19 16:27:58 +0500

    change DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { ScheduleModule } from '@nestjs/schedule';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb+srv://user123:sdbs312@cluster0.6ggsm1q.mongodb.net/DB6?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot('mongodb+srv://user123:uUWFZVkTxU3MaYNX@cluster0.6ggsm1q.mongodb.net/DB6?retryWrites=true&w=majority'),
     GroupModule,
     ScheduleModule.forRoot(),
     SeedModule,
--- src/db/database.providers.ts
+++ src/db/database.providers.ts
@@ -1,9 +1,19 @@
 import * as mongoose from 'mongoose';
 
 export const databaseProviders = [
-    {
-        provide: 'DATABASE_CONNECTION',
-        useFactory: (): Promise<typeof mongoose> =>
-            mongoose.connect('mongodb+srv://new_user_blya:2AKHOE1lpZYSoPru@cluster0.lgtepjy.mongodb.net/?retryWrites=true&w=majority'),
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async (): Promise<typeof mongoose> => {
+      try {
+        const connection = await mongoose.connect(
+            'mongodb+srv://user123:uUWFZVkTxU3MaYNX@cluster0.6ggsm1q.mongodb.net/DB6?retryWrites=true&w=majority&authSource=admin&readPreference=primary'
+        );
+        console.log('Connected to MongoDB successfully');
+        return connection;
+      } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        throw err; // Важно: выбрасываем ошибку, чтобы система обработала её
+      }
     },
+  },
 ];
