2a6f8b3fabb633a44f75d828dce87c1151494503
torekhanmukhtarov <nahkerot@gmail.com>
2024-11-22 01:12:08 +0500

    added all filters

--- src/filter/filter.controller.ts
+++ src/filter/filter.controller.ts
@@ -1,14 +1,22 @@
-import {Controller, Get, Query} from '@nestjs/common';
-import {FilterProductsDto} from "./dto/FilterProductsDto";
-import {FilterService} from "./filter.service";
+import { Controller, Get, Query } from '@nestjs/common';
+import { Auth } from 'src/iam/authentication/decorator/auth.decorator';
+import { FilterProductsDto } from './dto/FilterProductsDto';
+import { FilterService } from './filter.service';
+import { AuthType } from 'src/iam/authentication/enum/auth-type.enum';
 
+@Auth(AuthType.None)
 @Controller('filter')
 export class FilterController {
-    constructor(private readonly filterService: FilterService) {}
+  constructor(private readonly filterService: FilterService) {}
 
-    @Get('products')
-    async findProductsByFilter(@Query() filterProductsDto: FilterProductsDto) {
-        console.log(filterProductsDto);
-        return await this.filterService.findByFilters(filterProductsDto);
-    }
+  @Get('products')
+  async findProductsByFilter(@Query() filterProductsDto: FilterProductsDto) {
+    console.log(filterProductsDto);
+    return await this.filterService.findByFilters(filterProductsDto);
+  }
+
+  @Get('all-filters')
+  async findAllFilters() {
+    return await this.filterService.findAllFiltersFromGroups();
+  }
 }
--- src/filter/filter.service.ts
+++ src/filter/filter.service.ts
@@ -1,59 +1,100 @@
-import {Injectable, InternalServerErrorException} from "@nestjs/common";
-import {InjectModel} from "@nestjs/mongoose";
-import {Model, Types} from 'mongoose';
-import {Filter} from "./filter.group";
-import {FilterProductsDto} from "./dto/FilterProductsDto";
-import {Product} from "../products/products.schema";
-import {IGroup} from "../groups/interface/IGroup";
-import {Group} from "../groups/group.schema";
-import {Marker} from "../marker/marker.schema";
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Filter } from './filter.group';
+import { FilterProductsDto } from './dto/FilterProductsDto';
+import { Product } from '../products/products.schema';
+import { IGroup } from '../groups/interface/IGroup';
+import { Group } from '../groups/group.schema';
+import { Marker } from '../marker/marker.schema';
 
 @Injectable()
 export class FilterService {
-    constructor(
-        @InjectModel(Group.name) private groupModel: Model<IGroup>,
-        @InjectModel(Product.name) private productModel: Model<Product>,
-        @InjectModel(Marker.name) private markerModel: Model<Marker>, // Добавляем модель маркера
-        @InjectModel(Filter.name) private filterModel: Model<Filter>
-    ) {}
+  constructor(
+    @InjectModel(Group.name) private groupModel: Model<IGroup>,
+    @InjectModel(Product.name) private productModel: Model<Product>,
+    @InjectModel(Marker.name) private markerModel: Model<Marker>, // Добавляем модель маркера
+    @InjectModel(Filter.name) private filterModel: Model<Filter>,
+  ) {}
 
-    async findByFilters(filterDto: FilterProductsDto): Promise<Product[]> {
-        const filterId = new Types.ObjectId(filterDto.filterId);
-        const groupId = new Types.ObjectId(filterDto.groupId);
+  async findByFilters(filterDto: FilterProductsDto): Promise<Product[]> {
+    const filterId = new Types.ObjectId(filterDto.filterId);
+    const groupId = new Types.ObjectId(filterDto.groupId);
 
-        const group = await this.groupModel.findById(groupId).exec();
-        if (!group) {
-            throw new Error('Group not found');
-        }
-        
-        const filter = await this.filterModel.findById(filterId).exec();
-        if (!filter) {
-            throw new Error('Filter not found');
-        }
-
-        // Шаг 2: Фильтровать маркеры в приложении
-        const markers = await this.markerModel.find({ secondaryWord: filter.primaryWord }).exec();
-        if (markers.length === 0) {
-            return [];
-        }
-
-        const markerIds = markers.map(marker => marker._id);
-        console.log('markerIds', markerIds)
-
-        // Шаг 3: Ищем продукты, содержащие эти маркеры
-        const products = await this.productModel.find({
-            "_id": { $in: group.items },
-            "markers": { $in: markerIds }
-        }).exec();
-        
-        return products;
+    const group = await this.groupModel.findById(groupId).exec();
+    if (!group) {
+      throw new Error('Group not found');
     }
 
-    async listAllFilters(): Promise<Filter[]> {
-        try {
-            return await this.filterModel.find().exec();
-        } catch (error) {
-            throw new InternalServerErrorException('Ошибка при получении списка фильтров');
-        }
+    const filter = await this.filterModel.findById(filterId).exec();
+    if (!filter) {
+      throw new Error('Filter not found');
     }
+
+    // Шаг 2: Фильтровать маркеры в приложении
+    const markers = await this.markerModel
+      .find({ secondaryWord: filter.primaryWord })
+      .exec();
+    if (markers.length === 0) {
+      return [];
+    }
+
+    const markerIds = markers.map((marker) => marker._id);
+    console.log('markerIds', markerIds);
+
+    // Шаг 3: Ищем продукты, содержащие эти маркеры
+    const products = await this.productModel
+      .find({
+        _id: { $in: group.items },
+        markers: { $in: markerIds },
+      })
+      .exec();
+
+    return products;
+  }
+
+  async listAllFilters(): Promise<Filter[]> {
+    try {
+      return await this.filterModel.find().exec();
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Ошибка при получении списка фильтров',
+      );
+    }
+  }
+
+  async findAllFiltersFromGroups(): Promise<any[]> {
+    try {
+      // 1. Получить все группы
+      const groups = await this.groupModel.find({}, 'filters').exec();
+  
+      // 2. Собрать уникальные filterIds из всех групп
+      const filterIds = [
+        ...new Set(groups.flatMap((group) => group.filters.map((filterId) => filterId.toString()))),
+      ];
+  
+      if (filterIds.length === 0) {
+        return []; // Если фильтров нет, возвращаем пустой массив
+      }
+  
+      // 3. Преобразовать filterIds в ObjectId
+      const objectIdFilterIds = filterIds.map((id) => new Types.ObjectId(id));
+  
+      // 4. Получить фильтры по filterIds
+      const filters = await this.filterModel
+        .find({ _id: { $in: objectIdFilterIds } })
+        .exec();
+  
+      // 5. Вернуть данные фильтров
+      const result = filters.map((filter) => ({
+        _id: filter._id.toString(),
+        primaryWord: filter.primaryWord.trim(), // Убираем лишние пробелы из primaryWord
+      }));
+  
+      return result;
+    } catch (error) {
+      console.error('Error in findAllFiltersFromGroups:', error);
+      throw new InternalServerErrorException('Error while fetching all filters from groups');
+    }
+  }
 }
