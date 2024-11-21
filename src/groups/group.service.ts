e12f12d319bbd661d08ad5cd20ba5e751bc53879
torekhanmukhtarov <nahkerot@gmail.com>
2024-11-21 20:07:02 +0500

    added models

--- src/groups/group.service.ts
+++ src/groups/group.service.ts
@@ -1,96 +1,113 @@
-import {Injectable, InternalServerErrorException} from "@nestjs/common";
-import {InjectModel} from "@nestjs/mongoose";
-import {Group} from "./group.schema";
-import {Model, Types} from "mongoose";
-import {FilterProductsDto} from "../filter/dto/FilterProductsDto";
-import {Product} from "../products/products.schema";
-import {FindGroupDto} from "./group.dto/group.dto";
-import {IGroup} from "./interface/IGroup";
-import {FilterService} from "../filter/filter.service";
-import {MarkerService} from "../marker/marker.service";
-import {ProductsService} from "../products/products.service";
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Group } from './group.schema';
+import { Model, Types } from 'mongoose';
+import { FilterProductsDto } from '../filter/dto/FilterProductsDto';
+import { Product } from '../products/products.schema';
+import { FindGroupDto } from './group.dto/group.dto';
+import { IGroup } from './interface/IGroup';
+import { FilterService } from '../filter/filter.service';
+import { MarkerService } from '../marker/marker.service';
+import { ProductsService } from '../products/products.service';
 
 @Injectable()
 export class GroupService {
-    constructor(
-        @InjectModel(Group.name) private groupModel: Model<IGroup>,
-        @InjectModel(Product.name) private productModel: Model<Product>,
-        private filterService: FilterService,
-        private markerService: MarkerService,
-        private productService: ProductsService
-    ) {}
+  constructor(
+    @InjectModel(Group.name) private groupModel: Model<IGroup>,
+    @InjectModel(Product.name) private productModel: Model<Product>,
+    private filterService: FilterService,
+    private markerService: MarkerService,
+    private productService: ProductsService,
+  ) {}
 
-    async findByIdWithProducts(findGroupDto: FindGroupDto): Promise<Group | null> {
-        try {
-            const objectId = new Types.ObjectId(findGroupDto.id);
-            const result = await this.groupModel.findOne({ "_id": objectId }).populate('items').exec();
+  async findByIdWithProducts(
+    findGroupDto: FindGroupDto,
+  ): Promise<Group | null> {
+    try {
+      const objectId = new Types.ObjectId(findGroupDto.id);
+      const result = await this.groupModel
+        .findOne({ _id: objectId })
+        .populate('items')
+        .exec();
 
-            if (result) {
-                const fullResult: Group = result.toObject();
+      if (result) {
+        const fullResult: Group = result.toObject();
 
-                return fullResult;
-            } else {
-                return null;
-            }
-        } catch (error) {
-            throw new InternalServerErrorException('Ошибка при выполнении запроса');
-        }
+        return fullResult;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      throw new InternalServerErrorException('Ошибка при выполнении запроса');
     }
-    async listAllGroups(): Promise<IGroup[]> {
-        return this.groupModel.find().populate('items').exec();
+  }
+  async listAllGroups(): Promise<IGroup[]> {
+    return this.groupModel.find().populate('items').exec();
+  }
+
+  async listAllWithData(): Promise<any[]> {
+    try {
+      const groups = await this.listAllGroups();
+      const filters = await this.filterService.listAllFilters();
+      const markers = await this.markerService.listAllMarkers();
+      const products = await this.productService.listAllProducts(); // Получение всех продуктов
+
+      return groups.map((group) => {
+        const groupFilters = filters.filter(
+          (filter) =>
+            Array.isArray(group.filters) &&
+            group.filters.some((groupFilter) =>
+              groupFilter._id.equals(filter._id),
+            ),
+        );
+
+        // Предполагается, что group.items содержит идентификаторы продуктов
+        const enrichedProducts = products
+          .filter((product) =>
+            group.items.some((itemId) => itemId.equals(product._id)),
+          )
+          .map((product) => {
+            const productMarkers = markers.filter(
+              (marker) =>
+                Array.isArray(product.markers) &&
+                product.markers.some((productMarker) =>
+                  productMarker._id.equals(marker._id),
+                ),
+            );
+
+            return {
+              _id: product._id.toString(),
+              name: product.name,
+              productModel: product.productModel,
+              price: product.price,
+              mainPhoto: product.mainPhoto,
+              photos: product.photos,
+              temperature: product.temperature,
+              technology: product.technology,
+              hairColor: product.hairColor,
+              skinColor: product.skinColor,
+              details: product.details,
+              application: product.application,
+              composition: product.composition,
+              relatedProducts: product.relatedProducts
+                ? product.relatedProducts.map((rp) => rp._id.toString())
+                : [],
+              markers: productMarkers.map((marker) => ({
+                _id: marker._id.toString(),
+                secondaryWord: marker.secondaryWord,
+              })),
+            };
+          });
+
+        return {
+          ...group.toJSON(),
+          filters: groupFilters,
+          items: enrichedProducts,
+        };
+      });
+    } catch (error) {
+      console.error('Error fetching all data:', error);
+      throw new InternalServerErrorException('Error fetching all data');
     }
-
-    async listAllWithData(): Promise<any[]> {
-        try {
-            const groups = await this.listAllGroups();
-            const filters = await this.filterService.listAllFilters();
-            const markers = await this.markerService.listAllMarkers();
-            const products = await this.productService.listAllProducts(); // Получение всех продуктов
-
-            return groups.map(group => {
-                const groupFilters = filters.filter(filter =>
-                    Array.isArray(group.filters) && group.filters.some(groupFilter => groupFilter._id.equals(filter._id))
-                );
-
-                // Предполагается, что group.items содержит идентификаторы продуктов
-                const enrichedProducts = products.filter(product =>
-                    group.items.some(itemId => itemId.equals(product._id))
-                ).map(product => {
-                    const productMarkers = markers.filter(marker =>
-                        Array.isArray(product.markers) && product.markers.some(productMarker => productMarker._id.equals(marker._id))
-                    );
-
-                    return {
-                        _id: product._id.toString(),
-                        name: product.name,
-                        productModel: product.productModel,
-                        price: product.price,
-                        mainPhoto: product.mainPhoto,
-                        photos: product.photos,
-                        temperature: product.temperature,
-                        technology: product.technology,
-                        hairColor: product.hairColor,
-                        skinColor: product.skinColor,
-                        details: product.details,
-                        application: product.application,
-                        composition: product.composition,
-                        relatedProducts: product.relatedProducts ? product.relatedProducts.map(rp => rp._id.toString()) : [],
-                        markers: productMarkers.map(marker => ({
-                            _id: marker._id.toString(),
-                            secondaryWord: marker.secondaryWord
-                        }))
-                    };
-                });
-
-                return {
-                    ...group.toJSON(),
-                    filters: groupFilters,
-                    items: enrichedProducts
-                };
-            });
-        } catch (error) {
-            console.error('Error fetching all data:', error);
-            throw new InternalServerErrorException('Error fetching all data');
-        }
-    }
+  }
 }
--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,35 +1,53 @@
-import {Controller, Get, InternalServerErrorException, Param} from "@nestjs/common";
-import {FindGroupDto} from "./group.dto/group.dto";
-import {PipelineStage} from "mongoose";
-import {GroupService} from "./group.service";
-import {Group} from "./group.schema";
-import {IGroup} from "./interface/IGroup";
-import { AuthType } from "src/iam/authentication/enum/auth-type.enum";
-import { Auth } from "src/iam/authentication/decorator/auth.decorator";
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
+import { FindGroupDto } from './group.dto/group.dto';
+import { PipelineStage } from 'mongoose';
+import { GroupService } from './group.service';
+import { Group } from './group.schema';
+import { IGroup } from './interface/IGroup';
+import { AuthType } from 'src/iam/authentication/enum/auth-type.enum';
+import { Auth } from 'src/iam/authentication/decorator/auth.decorator';
 
 @Auth(AuthType.None)
 @Controller('Groups')
 export class GroupsController {
-    constructor(private readonly groupsService: GroupService) {}
+  constructor(private readonly groupsService: GroupService) {}
 
-    @Get(':id')
-    async findOne(@Param('id') id: string): Promise<Group> {
-        const findGroupDto = { id }; // Предполагается, что у вас есть DTO с полем id
-        try {
-            const result = await this.groupsService.findByIdWithProducts(findGroupDto);
-            return result;
-        } catch (error) {
-            console.error('Error fetching group with products:', error);
-            throw new InternalServerErrorException('Error fetching group with products');
-        }
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Group> {
+    const findGroupDto = { id };
+    try {
+      const result =
+        await this.groupsService.findByIdWithProducts(findGroupDto);
+      if (!result) {
+        throw new NotFoundException(`Group with id ${id} not found`);
+      }
+      return result;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      console.error('Error fetching group with products:', error);
+      throw new InternalServerErrorException(
+        'Error fetching group with products',
+      );
     }
-    @Get()
-    async listAll(): Promise<any[]> { // Изменено возвращаемое значение на Promise<any[]>
-        try {
-            return await this.groupsService.listAllWithData(); // Вызов метода listAllWithData
-        } catch (error) {
-            console.error('Error fetching all groups data:', error);
-            throw new InternalServerErrorException('Error fetching all groups data');
-        }
+  }
+  @Get()
+  async listAll(): Promise<any[]> {
+    // Изменено возвращаемое значение на Promise<any[]>
+    try {
+      const data = await this.groupsService.listAllWithData();
+      return data;
+      // Вызов метода listAllWithData
+    } catch (error) {
+      console.error('Error fetching all groups data:', error);
+      throw new InternalServerErrorException('Error fetching all groups data');
     }
+  }
 }
