c30a01085dcea1708f92240300e3cc22be6bc497
torekhanmukhtarov <nahkerot@gmail.com>
2024-11-22 10:43:11 +0500

    added logs for main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 
+console.log('Environment variables:', {
+  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV
+});
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
